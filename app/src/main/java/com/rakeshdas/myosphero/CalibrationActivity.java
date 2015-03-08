4ea7efaa959ae2e7a8b7e851b28414665fd54e8b
Rakesh Das <rakeshdaskowshik@gmail.com>
2015-03-07 18:21:36 -0600

    Calibration

--- app/src/main/java/com/rakeshdas/myosphero/CalibrationActivity.java
+++ app/src/main/java/com/rakeshdas/myosphero/CalibrationActivity.java
@@ -2,29 +2,40 @@ package com.rakeshdas.myosphero;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import orbotix.view.calibration.CalibrationButtonView;
 import orbotix.view.calibration.CalibrationView;
 
 
 public class CalibrationActivity extends Activity {
-private CalibrationView mCalibrationView;
+    private CalibrationView mCalibrationView;
+    private CalibrationButtonView mCalibrationButtonViewAbove;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibration);
-        mCalibrationView = (CalibrationView)findViewById(R.id.CalibrationView);
+        //mCalibrationView = (CalibrationView)findViewById(R.id.calibration_above);
+        mCalibrationButtonViewAbove = (CalibrationButtonView)findViewById(R.id.calibration_button_above);
         Intent calibrationIntent = getIntent();
         Bundle bundle = getIntent().getExtras();
         //Sphero mRobot = (Sphero) bundle.getParcelable("Robot");
-        mCalibrationView = (CalibrationView)findViewById(R.id.CalibrationView);
+       /* mCalibrationView = (CalibrationView)findViewById(R.id.CalibrationView);
         mCalibrationView.setColor(Color.WHITE);
         mCalibrationView.setCircleColor(Color.WHITE);
         //mCalibrationView.setRobot(mRobot);
-        mCalibrationView.enable();
+        mCalibrationView.enable();*/
+        // Initialize calibrate button view where the calibration circle shows above button
+        // This is the default behavior
+        mCalibrationButtonViewAbove = (CalibrationButtonView)findViewById(R.id.calibration_above);
+        mCalibrationButtonViewAbove.setCalibrationButton((View)findViewById(R.id.calibration_button_above));
+        // You can also change the size of the calibration views
+        mCalibrationButtonViewAbove.setRadius(300);
+        mCalibrationButtonViewAbove.setCalibrationCircleLocation(CalibrationButtonView.CalibrationCircleLocation.ABOVE);
     }
 
 
